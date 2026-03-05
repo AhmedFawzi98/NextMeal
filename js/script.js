05d2a59b14912044ad0c12b3d0b86c063d61c9cf
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-03-05 23:00:38 +0200

    Add Hamburger menu

--- js/script.js
+++ js/script.js
@@ -2,11 +2,35 @@
 
 /* Toggle Password plain text / masked*/
 const input = document.getElementById("password");
-const toggle = document.querySelector(".toggle-password");
+const togglePasswordBtn = document.querySelector(".toggle-password");
 
-toggle.addEventListener("click", () => {
+togglePasswordBtn.addEventListener("click", () => {
     const isPassword = input.type === "password";
 
     input.type = isPassword ? "text" : "password";
-    toggle.classList.toggle("is-visible");
+    togglePasswordBtn.classList.toggle("is-visible");
+});
+
+/* toggle hamburger menu */
+const toggleHamburgerMenuBtn = document.querySelector(".toggle-hamburger");
+const header = document.querySelector(".section-header");
+const navCTA = document.querySelector(".header-nav-link.nav-cta");
+const colorTextOnPrimary = getComputedStyle(
+    document.documentElement,
+).getPropertyValue("--color-text-on-primary");
+const colorPrimary = getComputedStyle(
+    document.documentElement,
+).getPropertyValue("--color-primary");
+
+toggleHamburgerMenuBtn.addEventListener("click", () => {
+    header.classList.toggle("nav-open");
+    if (navCTA.classList.contains("nav-cta")) {
+        navCTA.classList.remove("nav-cta");
+        navCTA.classList.add("btn", "btn-cta-primary");
+        navCTA.style.color = colorTextOnPrimary;
+    } else {
+        navCTA.classList.add("nav-cta");
+        navCTA.classList.remove("btn", "btn-cta-primary");
+        navCTA.style.color = colorPrimary;
+    }
 });
