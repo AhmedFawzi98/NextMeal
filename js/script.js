2ee84d30ff66f59adfd83166602343fb8c53e958
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-03-07 23:41:10 +0200

    Add media quries for smaller screens (small tablets and phones)

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ togglePasswordBtn.addEventListener("click", () => {
     togglePasswordBtn.classList.toggle("is-visible");
 });
 
-/* toggle hamburger menu */
+/* hamburger menu */
 const toggleHamburgerMenuBtn = document.querySelector(".toggle-hamburger");
 const header = document.querySelector(".section-header");
 const navCTA = document.querySelector(".header-nav-link.nav-cta");
+const headerNavLinks = document.querySelectorAll(".header-nav-link");
 const colorTextOnPrimary = getComputedStyle(
     document.documentElement,
 ).getPropertyValue("--color-text-on-primary");
@@ -33,4 +34,36 @@ toggleHamburgerMenuBtn.addEventListener("click", () => {
         navCTA.classList.remove("btn", "btn-cta-primary");
         navCTA.style.color = colorPrimary;
     }
+});
+
+headerNavLinks.forEach((headerNavLink) => {
+    headerNavLink.addEventListener("click", () => {
+        header.classList.remove("nav-open");
+    });
+});
+
+/*adjust copyright year*/
+const currentYearSpan = document.querySelector(".current-year");
+const currentYear = new Date().getFullYear().toString();
+currentYearSpan.textContent = currentYear;
+
+/* smooth scrolling*/
+const allLinks = document.querySelectorAll("a");
+allLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault(); //prevent default behaviour of instant go to.
+        // const href = link.href; //this will return full url! http://localhost:5000/#  we don't want full url!
+        const href = link.getAttribute("href");
+        if (href === "#") {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        } else if (href !== "#" && href.startsWith("#")) {
+            const section = document.querySelector(href);
+            section.scrollIntoView({
+                behavior: "smooth",
+            });
+        }
+    });
 });
